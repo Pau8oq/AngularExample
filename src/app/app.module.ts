206b347e0088ffedd8ce8bab0d6a48e3cdb45d1d
Vasyl Uhryn <ugrinvasil@gmail.com>
2020-02-28 11:09:58 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TmpComponent } from './tmp/tmp.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      TmpComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/tmp/tmp.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'tmp-comp',
+    templateUrl: './tmp.component.html',
+    styleUrls: ['./tmp.component.scss']
+})
+export class TmpComponent {
+
+    title = "tmp-component form source"
+}
