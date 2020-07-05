f27f48a5ffdd9832f600ca1b5ea510c18bc277a5
Vasyl Uhryn <ugrinvasil@gmail.com>
2020-07-05 20:28:30 +0300

    last  update  before windows reinstallaton

--- src/app/tmp/tmp.component.ts
+++ src/app/tmp/tmp.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { isNumber } from 'util';
 
 @Component({
     selector: 'tmp-comp',
@@ -10,12 +11,31 @@ import { Subscription } from 'rxjs';
 export class TmpComponent {
 
     id: number;
-    title = "tmp-component form source"
-    private subscription: Subscription;
-    //https://metanit.com/web/angular2/7.3.php
-    constructor(private activeRoute: ActivatedRoute){
-        //this.id = activeRoute.snapshot.params['id'];
-        this.subscription = activeRoute.params.subscribe(params=>this.id=params['id']);
+    price: number;
+    title = "tmp-component form source";
+    
 
+    private routeSubscription: Subscription;
+    //https://metanit.com/web/angular2/7.3.php
+    private querySubscription: Subscription;
+
+    constructor(private route: ActivatedRoute){
+        //this.id = activeRoute.snapshot.params['id'];
+        this.routeSubscription = route.params.subscribe(params=>this.id=params['id']);
+
+        //The plus (+params['id']) you are referring to is a standard javaScript operator that
+        //tells the interpreter to convert the param result from whatever type it is (string) into a number
+        this.querySubscription = route.queryParams.subscribe(queryParams =>
+            {
+                //проблема тут щоо якщо +true = 1
+                //this.price=+queryParams['price'];
+                if(Number(queryParams['price'])){
+                    this.price=queryParams['price'];
+                }
+                else{
+                    this.price = -1;
+                }
+                
+            });
     }
 }
